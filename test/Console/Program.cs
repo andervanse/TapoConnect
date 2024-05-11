8576043fd1b2cf7553481560757ec926b9fb70c7
andervanse <andervanse@gmail.com>
2024-05-11 11:25:13 +0100

    output of console test improved.

--- test/Console/Program.cs
+++ test/Console/Program.cs
@@ -33,9 +33,23 @@ internal class Program
 
         Console.WriteLine("************* BEGIN *************");
         Console.WriteLine("Logging in to the cloud");
-        var result = await cloudClient.LoginAsync(authConfig.Email, authConfig.Password);
-        Console.WriteLine("Account id.: {0}", result.AccountId);
-        Console.WriteLine("Nickname...: {0}", result.Nickname);
+        var loginResult = await cloudClient.LoginAsync(authConfig.Email, authConfig.Password);
+        Console.WriteLine("Account id.: {0}", loginResult.AccountId);
+        Console.WriteLine("Nickname...: {0}", loginResult.Nickname);
+        Console.WriteLine("************ DEVICES ************");
+        var devicesResponse = await cloudClient.ListDevicesAsync(loginResult.Token);
+
+        bool firstRow = true;
+        foreach(var device in devicesResponse.DeviceList)
+        {
+            if (!firstRow)
+                Console.WriteLine("----------------------------------");
+            Console.WriteLine("Alias.: {0}", device.Alias);
+            Console.WriteLine("Model.: {0}", device.DeviceModel);
+            Console.WriteLine("Mac...: {0}", device.DeviceMac);
+            firstRow = false;
+        }
+
         Console.WriteLine("*************  END  *************");
     }
 }
